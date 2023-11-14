e9ef8cfc496f7064859c1c98bc1808cc69e64e9d
Tony Padmore <emperialace@gmail.com>
2023-11-13 19:41:30 -0500

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Financial Tracker
+        </p>
+      </header>
+
+        <div className= "Bill-container">
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
