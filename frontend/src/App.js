74675822d2c1728cb645462d518e62a5596b6011
Tony Padmore <emperialace@gmail.com>
2023-11-24 15:38:59 -0500

    label and balance changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [balance, setBalance] = useState(0);
+
+  function addItem()
+  {
+      document.getElementById('addBill').style.display = 'block';
+  }
+
+    function closeItem()
+    {
+        document.getElementById('closeBill').style.display = 'none';
+    }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +23,28 @@ function App() {
           Financial Tracker
         </p>
       </header>
+        <div className= "balance">
+            ${balance}
+        </div>
+
+        <div>
+            <button onClick={addItem}>Add item</button>
+        </div>
+
+        <div>
+            <form id="addBill">
+
+               <label> Bill name: </label>
+                <input type={"text"}/>
+
+
+                <label> Bill cost: </label>
+                <input type={"text"}/>
+
+                <button onClick={closeItem}>X</button>
+
+            </form>
+        </div>
 
         <div className= "Bill-container">
 
